1ce647592ecfb41f5b60e40f4d3d69739c9fc60e
Lucas Micheleto <lucasnovo@outlook.com>
2023-10-09 23:50:19 -0300

    change auth event

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,7 +3,7 @@ import { User } from './user.model';
 import { AuthData } from './auth-data.model';
 import { Subject } from 'rxjs';
 import { Router } from '@angular/router';
-import { getAuth, createUserWithEmailAndPassword, Auth, signInWithEmailAndPassword, signOut } from '@angular/fire/auth';
+import { getAuth, createUserWithEmailAndPassword, Auth, signInWithEmailAndPassword, signOut, authState } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +13,24 @@ export class AuthService {
   authChange = new Subject<boolean>();
   private isAuthenticated = false;
   constructor(private router: Router) { 
+    this.initAuthListener();
+  }
+
+  initAuthListener() {
+    authState(this.auth).subscribe({
+      next: user => {
+        if (user) {
+          this.isAuthenticated = true;
+          this.authChange.next(this.isAuthenticated);
+          this.router.navigate(['/training']);
+        }
+        else {
+          this.isAuthenticated = false;
+          this.authChange.next(this.isAuthenticated);
+          this.router.navigate(['/login'])
+        }
+      }
+    })
   }
 
   registerUser(authData: AuthData) {
@@ -20,16 +38,11 @@ export class AuthService {
     .then((userCredential) => {
       // The user has been created successfully.
       //console.log('User created: ', userCredential.user);
-      this.authSuccessfully();
     })
     .catch((error) => {
       // There was an error creating the user.
       console.error('Error creating user: ', error);
     });
-    // this.user = {
-    //   email: authData.email,
-    //   userId: Math.round(Math.random() * 10000).toString()
-    // }
   }
 
   login(authData: AuthData) {
@@ -37,33 +50,20 @@ export class AuthService {
     .then((userCredential) => {
       // The user has been created successfully.
       //console.log('Login user: ', userCredential.user);
-      this.authSuccessfully();
     })
     .catch((error) => {
       // There was an error creating the user.
       console.error('Error login user: ', error);
     });
-    // this.user = {
-    //   email: authData.email,
-    //   userId: Math.round(Math.random() * 10000).toString()
-    // }
-    this.authSuccessfully();
+
   }
 
   logout() {
     signOut(this.auth)
-    this.isAuthenticated = false;
-    this.authChange.next(this.isAuthenticated);
-    this.router.navigate(['/login'])
   }
 
   isAuth() {
     return this.isAuthenticated;
   }
 
-  private authSuccessfully() {
-    this.isAuthenticated = true;
-    this.authChange.next(this.isAuthenticated);
-    this.router.navigate(['/training']);
-  }
 }
